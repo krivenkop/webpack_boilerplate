2b05da11d9edf7b7d0eff2a3feb69ca522cc691c
krivenkop <ukr.empresario@gmail.com>
2019-02-15 20:02:47 +0200

    ADD: sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,65 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: [
-    '.src/js/app.js',
+    './src/js/app.js',
+    './src/scss/main.scss',
   ],
   output: {
     filename: './js/bundle.js',
   },
   devtool: 'sourcemap',
   module: {
-    rules: [{
-      test: /\.js$/,
-      include: path.resolve(__dirname, 'src/js'),
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: 'env',
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/js'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
         },
       },
-    }],
+      {
+        test: /\.(scss|sass)$/,
+        include: path.resolve(__dirname, 'src/scss'),
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'resolve-url-loader',
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        }),
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: { name: 'img/[name].[ext]' },
+          },
+        ],
+      },
+    ],
   },
-  plugins: [],
+  plugins: [
+    new ExtractTextPlugin({
+      filename: './css/style.bundle.css',
+      allChunks: true,
+    }),
+  ],
 };
